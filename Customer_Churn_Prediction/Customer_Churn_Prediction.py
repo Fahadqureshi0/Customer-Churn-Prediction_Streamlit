43aa25da6787b88f73acdb5900f16d83780884ad
Fahad Qureshi <qureshifahad984@gmail.com>
2026-08-03 22:43:41 +0500

    Implement Streamlit UI for customer churn prediction
    Added Streamlit interface for customer churn prediction and updated model loading path.

--- Customer_Churn_Prediction/Customer_Churn_Prediction.py
+++ Customer_Churn_Prediction/Customer_Churn_Prediction.py
@@ -4,7 +4,7 @@ import pickle
 
 # Loading Model
 
-loaded_model = pickle.load(open('F:/Machine Learning Projects/Customer_Churn_Prediction/Trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('/Customer_Churn_Prediction/Trained_model.sav', 'rb'))
 
 def customer_churn_prediction():
     input_data = (23, 1, 3, 5, 9, 25, 0, 0, 120, 29)
@@ -19,10 +19,51 @@ def customer_churn_prediction():
     print(prediction)
 
     if (prediction[0] == 0):
-       return('Customer Did not Churn')
+       return(' 📉 Customer Did not Churn')
     else:
-       return('Customer Churn')
+       return(' 🚶‍♀️ Customer Churn')
 
 
 result = customer_churn_prediction()
 print(result)
+
+
+# ---------------Streamlit---------------#
+
+def main():
+
+   st.title(' 👤 Customer Churn Prediction')
+
+   Age = st.text_input('Person Age')
+   Gender = st.text_input('Person Gender')
+   Tenure = st.text_input('Tenure Value')
+   Usage_Frequency = st.text_input('Usage Frequency Value')
+   Support_Calls = st.text_input('Support Call Value')	 
+   Payment_Delay = st.text_input('Payment Delay Value')
+   Subscription_Type = st.text_input('Subscription Type')
+   Contract_Length = st.text_input('Contract Length')
+   Total_Spend = st.text_input('Total Spend Value')
+   Last_Interaction = st.text_input('Last Interaction')
+
+
+   if st.button('Predict'):
+
+      result = customer_churn_prediction([
+         float(Age),
+         float(Gender),
+         float(Tenure),
+         float(Usage_Frequency),
+         float(Support_Calls),
+         float(Payment_Delay),
+         float(Subscription_Type),
+         float(Contract_Length),
+         float(Total_Spend),
+         float(Last_Interaction)
+      ])
+
+      st.success(result)
+
+
+if __name__ == "__main__":
+   main()
+
